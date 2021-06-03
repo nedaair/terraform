b6ec0c9574567800ea306b0b89cba5924723ed26
nedaair <berserk18@gmail.com>
2021-06-03 22:13:04 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "this" {
 
 }
 
-resource "aws_security_group_rule" "this" {
+resource "aws_security_group_rule" "sgingress" {
   from_port = 8080
   to_port = 8080
   protocol = "tcp"
@@ -17,7 +17,7 @@ resource "aws_security_group_rule" "this" {
   type = "ingress"
 }
 
-resource "aws_security_group_rule" "this" {
+resource "aws_security_group_rule" "sgegress" {
   from_port = 0
   protocol = "all"
   security_group_id = aws_security_group.this.id
