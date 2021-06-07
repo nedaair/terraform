d57b1350e2191212f348d4f748a03eda315ad929
nedaair <berserk18@gmail.com>
2021-06-07 20:18:39 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,22 +5,9 @@ provider "aws" {
 }
 
 resource "aws_security_group" "this" {
+  name = "terraform_security"
 
-}
-
-resource "aws_security_group_rule" "sgingress" {
-  from_port = 8080
-  to_port = 8080
-  protocol = "tcp"
-  security_group_id = aws_security_group.this.id
-
-  type = "ingress"
-}
-
-resource "aws_security_group_rule" "sgegress" {
-  from_port = 0
-  protocol = "all"
-  security_group_id = aws_security_group.this.id
-  to_port = 0
-  type = "egress"
+  tags = {
+    "managedby1" : "terraform"
+  }
 }
