2cf7fdb67090767a403daff01207901c9f173470
nedaair <berserk18@gmail.com>
2021-06-07 20:32:15 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,28 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_security_group" "this" {
-  name = "terraform_security"
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
-    "managedby1" : "terraform"
+    Name = "HelloWorld"
   }
 }
